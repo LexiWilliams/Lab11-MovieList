876c4696fcfbbd514912a2bc89d67e612c2be7e4
Lexi <lexa.shelby@gmail.com>
2019-07-29 17:27:51 -0400

    Lab 11 movie list first commit

---
+++ Lab11-MovieList/Movie.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lab11_MovieList
+{
+    class Movie
+    {
+        #region fields and properties
+        public string Title { get; set; }
+        public string Category { get; set; }
+        #endregion
+        #region Constructors
+        public Movie(string title, string category)
+        {
+            this.Title = title;
+            this.Category = category;
+        }
+        #endregion
+        #region Methods
+        public static List<Movie> Alphebatize(List<Movie> movies)
+        {
+
+            var moviesAlphabetical = movies.OrderBy(t => t.Title);
+            return moviesAlphabetical.ToList<Movie>();
+        }
+        public static void PrintMovies(List<Movie> movies, string message)
+        {
+            Console.WriteLine(message);
+            int count = 0;
+            foreach (Movie x in movies)
+            {
+                count++;
+                Console.WriteLine($"{count}.{x.Title}");
+            }
+            Console.WriteLine("");
+        }
+        public static int ChooseCategory(List<Movie> movies, string message)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine("1: Animations\n2.Comedies\n3.Documentaries\n");
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int categoryNumber))
+            {
+                if (categoryNumber > 0 && categoryNumber <= 3)
+                {
+                    return categoryNumber;
+                }
+            }
+            Console.WriteLine("That input is invalid");
+            return ChooseCategory(movies, message);
+        }
+        public static void PrintByCategory(int category, List<Movie> movies)
+        {
+            Console.WriteLine("");
+            if (category == 1)//animations
+            {
+                Console.WriteLine("Animations:");
+                foreach (Movie x in movies)
+                {
+                    if (x.Category == "Animation")
+                    {
+                        Console.WriteLine(x.Title);
+                    }
+                }
+                Console.WriteLine("");
+            }
+            else if (category == 2)//comedies
+            {
+                Console.WriteLine("Comedies:");
+                foreach (Movie x in movies)
+                {
+                    if (x.Category == "Comedy")
+                    {
+                        Console.WriteLine(x.Title);
+                    }
+                }
+                Console.WriteLine("");
+            }
+            else if (category == 3)//documentaries
+            {
+                Console.WriteLine("Documentaries:");
+                foreach (Movie x in movies)
+                {
+                    if (x.Category == "Documentary")
+                    {
+                        Console.WriteLine(x.Title);
+                    }
+                }
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine("That input is invalid");
+                PrintByCategory(category, movies);
+            }
+        }
+        public static bool RunAgain(string message)
+        {
+            Console.WriteLine(message);
+            string input = Console.ReadLine().ToLower();
+            if(input=="yes")
+            {
+                Console.WriteLine("");
+                return true;
+            }
+            else if(input == "no")
+            {
+                Console.WriteLine("Goodbye.");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("That input is invalid");
+                return RunAgain(message);
+            }
+        }
+    }
+    #endregion
+
+
+
+}
+
---
+++ Lab11-MovieList/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11_MovieList
+{
+    class Program
+    {
+        public static List<Movie> movies = new List<Movie>
+        {
+            new Movie("The Lion King", "Animation"),
+            new Movie("Mulan","Animation"),
+            new Movie("Free Solo","Documentary"),
+            new Movie("Role Models", "Comedy"),
+            new Movie("Blackfish","Documentary"),
+            new Movie("Zombieland","Comedy"),
+            new Movie("Aladdin","Animation"),
+            new Movie("The Hangover","Comedy"),
+            new Movie("Food, inc","Documentary"),
+            new Movie("Tangled","Animation")
+        };
+
+        static void Main(string[] args)
+        {
+
+            List<Movie> moviesAlphabetical = Movie.Alphebatize(movies);
+            bool runAgain = true;
+            while (runAgain)
+            {
+                int category = Movie.ChooseCategory(moviesAlphabetical, "Welcome to the movie list. There are 10 movies in this list." +
+                    "\nWhich category would you like to see movies for?");
+                Movie.PrintByCategory(category, moviesAlphabetical);
+                runAgain = Movie.RunAgain("Would you like to see movies in another category?\nType yes to choose another category, " +
+                    "type no to quit.");
+            }
+
+
+
+        }
+
+    }
+}
+    
+
